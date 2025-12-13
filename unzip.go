b7a7c7bd7c9e56725d1d50955f3e2d4233340605
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2025-12-13 20:56:35 +0100

    adding functions

--- unzip.go
+++ unzip.go
@@ -1,1 +1,9 @@
 package gzu
+
+func UnzipFromFile(srcFile, dstPath string, filter FilterFunc) error {
+	return nil
+}
+
+func UnzipFromBytes(data []byte, dstPath string, filter FilterFunc) error {
+	return nil
+}
--- zip.go
+++ zip.go
@@ -1,1 +1,9 @@
 package gzu
+
+func ZipToFile(srcPath, dstPath string, filter FilterFunc) error {
+	return nil
+}
+
+func ZipToBytes(srcPath string, filter FilterFunc) ([]byte, error) {
+	return nil, nil
+}
